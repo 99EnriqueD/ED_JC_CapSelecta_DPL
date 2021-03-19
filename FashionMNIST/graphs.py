c52c02b6f979a9a7952954445d514174df540670
JinfuC <jinfu.chen@student.kuleuven.be>
2021-03-19 16:57:33 +0100

    graph stuff

--- FashionMNIST/graphs.py
+++ FashionMNIST/graphs.py
@@ -1,14 +1,28 @@
 import matplotlib.pyplot as plt
 
+def save_data(iteration,data,file_name):
+        with open(file_name) as f:
+                f.write(iteration:data)
+        return
 
-
-def save_accuracy_data(iterations_base,accuracy_base,iterations_data,label,file_name):
-        plt.plot(iterations_base,accuracy_base,label=)
-
-
-def save_F1_data(data, file_name):
-
-
-def make_accuracy_plot(dpl_data_file,baseline_data_file,plot_file_name) :
-
-def make_F1_plot(dpl_data_file,baseline_data_file,plot_file_name) :
+def make_plot(dpl_data_file,baseline_data_file,label_base,label_dpl,output_file):
+        iterations_base= []
+        iterations_dpl= []
+        to_plot_base= []
+        to_plot_dpl= []
+        with open(dpl_data_file) as f:
+                line= f.readlines()
+                splitted= line.split(":")
+                iterations_dpl.append(int(splitted[0]))
+                to_plot_dpl.append(float(splitted[1]))
+        with open(base_data_file) as f:
+                line= f.readlines()
+                splitted= line.split(":")
+                iterations_base.append(int(splitted[0]))
+                to_plot_base.append(float(splitted[1]))
+        
+        plt.plot(iterations_base,to_plot_base, label=label_base)
+        plt.plot(iterations_base,to_plot_base, label=label_dpl)
+        plt.legend(loc='best')
+        plt.savefig(output_file)
+        return
