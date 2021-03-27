b7445686ae047cd6469866fd0881f0743c831fbe
JinfuC <43752841+JinfuC@users.noreply.github.com>
2021-03-27 14:57:54 +0100

    Update graphs.py

--- graphs/graphs.py
+++ graphs/graphs.py
@@ -54,8 +54,37 @@ def make_plot(dpl_data_file,baseline_data_file,label_base,label_dpl,x_axis_name,
         plt.clf()
         return
 
+
+def calc_diff(dpl_data_file,baseline_data_file):
+        baseline_data_file = rel_path + baseline_data_file
+        dpl_data_file = rel_path + dpl_data_file
+        iterations_base= []
+        iterations_dpl= []
+        to_plot_base= []
+        to_plot_dpl= []
+        with open(dpl_data_file) as f:
+                for line in f.readlines():
+                        splitted= line.split(":")
+                        iterations_dpl.append(int(splitted[0]))
+                        to_plot_dpl.append(float(splitted[1]))
+        with open(baseline_data_file) as f:
+                for line in f.readlines():
+                        splitted= line.split(":")
+                        iterations_base.append(int(splitted[0]))
+                        to_plot_base.append(float(splitted[1]))
+        
+        return iterations_base, (to_plot_dpl-to_plot_base)
+
+def make_diff_plot(dpl_data_file1,baseline_data_file1,dpl_data_file2,base_data_file2):
+        amount_iterations1, diff1= calc_diff(dpl_data_file1,baseline_data_file1)
+        amount_iterations2, diff2= calc_diff(dpl_data_file2,baseline_data_file2)
+        print(diff1)
+        print(diff2)
+        return
+
 #make_plot("outfit_acc.txt","outfit_baseline_acc.txt","Baseline outfit classifier","DeepProbLog outfit classifier","iterations","accuracy","outfit accuracy")
 #make_plot("outfit_F1.txt","outfit_baseline_F1.txt","Baseline outfit classifier","DeepProbLog outfit classifier","iterations","F1- score","outfit F1- score")
 #make_plot("budget_acc.txt","budget_baseline_acc.txt","Baseline budget classifier","DeepProbLog budget classifier","iterations","accuracy","budget accuracy")
 #make_plot("budget_F1.txt","budget_baseline_F1.txt","Baseline budget classifier","DeepProbLog budget classifier","iterations","F1- score","budget F1- score")
 #make_plot("outfit_dist.txt","outfit_baseline_dist.txt","Baseline budget classifier","DeepProbLog budget classifier","iterations","distance","budget distance function")
+make_diff_plot("outfit_F1.txt","outfit_baseline_F1.txt","budget_F1.txt","budget_baseline_F1.txt")
